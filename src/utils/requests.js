c2e7bdf973ff1749578315ba29a098d59235361e
jainsahab-1506 <2019UCP1390@mnit.ac.in>
2021-07-02 11:13:05 +0530

    covi

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,11 +1,10 @@
-export const SITE_URL = 'http://localhost:3000';
-export const SERVER_URL ="http://localhost:8000";
+export const SITE_URL = "http://localhost:3000";
+export const SERVER_URL = "http://localhost:8000";
 
 export const requests = {
-
-	doLogin: '/login',
-	doRegister: '/register',
-	addContact:"/contact",
-	getData:"https://api.covid19india.org/data.json",
-	subscribe:"/subscribe"
+  doLogin: "/login",
+  doRegister: "/register",
+  addContact: "/contact",
+  getData: "https://api.covid19india.org/data.json#",
+  subscribe: "/subscribe",
 };
