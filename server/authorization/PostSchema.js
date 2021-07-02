fbe06c71d040a3e14b99a208f431ebd44acb29c7
Raghav3018 <raghavbhandari3@gmail.com>
2021-07-02 11:11:36 +0530

    create post route

---
+++ server/authorization/PostSchema.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose");
+const User=require("./model");
+const postSchema=mongoose.Schema(
+    {
+      
+        message:{
+            type: String,
+            required:true
+        },
+        Date:{
+            type: String,
+            required:true
+        },
+        ownerid:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User',
+          
+        }
+    }
+)
+const Post=mongoose.model('Post', postSchema);
+
+module.exports=Post;
